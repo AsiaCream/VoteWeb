34893657170b7726c142887c6087c8380a7e68b6
AsiaCream <343224963@qq.com>
2017-01-03 18:32:18 +0800

    add photo.cs

--- VoteWeb/src/VoteWeb/Models/Photo.cs
+++ VoteWeb/src/VoteWeb/Models/Photo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,31 +8,35 @@ namespace VoteWeb.Models
     public class Photo
     {
         /// <summary>
-        /// 作品ID
+        /// 作者ID
         /// </summary>
         public long PhotoID { get; set; }
         /// <summary>
-        /// 作品标题
+        /// 作品名称
         /// </summary>
         public string Title { get; set; }
         /// <summary>
-        /// 类型ID
+        /// 作品地址
         /// </summary>
-        public long CategoryID { get; set; }
+        public string PhotoURL { get; set; }
         /// <summary>
         /// 作者ID
         /// </summary>
         public long AuthorID { get; set; }
         /// <summary>
-        /// 作品URL
+        /// 创建时间
         /// </summary>
-        public string PhotoURL { get; set; }
+        public DateTime CreateTime { get; set; }
         /// <summary>
         /// 作品简介
         /// </summary>
         public string Introduction { get; set; }
         /// <summary>
-        /// 是否已经删除
+        /// 是否展示
+        /// </summary>
+        public int IsDisplay { get; set; }
+        /// <summary>
+        /// 是否删除
         /// </summary>
         public int IsDelete { get; set; }
     }
