4fa8550783c168d58e0db2ecd21ae56ed4d88e3d
AsiaCream <343224963@qq.com>
2017-01-11 19:33:57 +0800

    init

---
+++ VoteWeb/src/VoteWeb/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using VoteWeb.Models;
+
+namespace VoteWeb.Controllers
+{
+    public class BaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+            //context.HttpContext.Connection.LocalIpAddress.MapToIPv4();
+        }
+    }
+}
